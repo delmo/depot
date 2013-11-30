3f7c1d718794d4209fab7809805ee54c00692abd
Ryan Delmo <rtgd20@sussex.ac.uk>
2013-11-30 00:40:47 +0000

    move cart to sidebar and use ajax to update

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -38,7 +38,9 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart} #optional , notice: 'Line item was successfully created.' }
+        format.html { redirect_to store_path} # remove to sidebar @line_item.cart} 
+         #optional , notice: 'Line item was successfully created.' }
+        format.js { @current_item = @line_item }
         format.json { render action: 'show', status: :created, location: @line_item }
       else
         format.html { render action: 'new' }
